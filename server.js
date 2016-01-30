1828558e84dafc818445c5741e4713bcb7fb02df
Akshay Kanthi <aksanoble@gmail.com>
2016-01-30 12:09:58 +0530

    add try-catch exception

--- server.js
+++ server.js
@@ -4,33 +4,33 @@
  *
  * Server code for periodically pulling data from APIs and providing it to infographic layout
  * 
-*/
-var express = require('express')
-var app = express()
-var request = require('superagent')
-var gApiToken = process.env.gApiToken
-var async = require('async')
-var appId = process.env.appId
-var cityCoordinates = require('./cities')
-var cities = Object.keys(cityCoordinates)
-var data
-var currentConditions = {}
-var pubnub = require("pubnub")({
-	ssl: true,
-	publish_key   : process.env.publish_key,
-	subscribe_key : process.env.subscribe_key
-});
+ */
+ var express = require('express')
+ var app = express()
+ var request = require('superagent')
+ var gApiToken = process.env.gApiToken
+ var async = require('async')
+ var appId = process.env.appId
+ var cityCoordinates = require('./cities')
+ var cities = Object.keys(cityCoordinates)
+ var data
+ var currentConditions = {}
+ var pubnub = require("pubnub")({
+ 	ssl: true,
+ 	publish_key   : process.env.publish_key,
+ 	subscribe_key : process.env.subscribe_key
+ });
 
 /*
  * refreshData
  * 
  * Initiates API calls for all cities one my one
  */
-function refreshData () {
-	data = {}
-	var i=0
-	fetch(cities[i], cityCoordinates[cities[i]], i, data)	
-}
+ function refreshData () {
+ 	data = {}
+ 	var i=0
+ 	fetch(cities[i], cityCoordinates[cities[i]], i, data)	
+ }
 
 /*
  * fetch
@@ -49,73 +49,83 @@ function refreshData () {
  * @param aggregate - aggregated data array for all cities
  * 
  */ 
-function fetch (place, coordinates, i, aggregate) {	
-	async.parallel([
-		function(callback) {
-			request
-			.get('https://maps.googleapis.com/maps/api/timezone/json')
-			.query('location='+coordinates[1]+','+coordinates[0])
-			.query({timestamp: Math.floor(new Date().getTime()/1000)})
-			.query({key: gApiToken})
-			.end(function(err, res){			
-				if(err) { console.log(err); callback(true); return; }
-				obj = res.body;
-				callback(false, obj);				
-			});
-		},
-		function(callback){
-			request
-			.get('http://api.sunrise-sunset.org/json')
-			.query({lat:coordinates[1]})
-			.query({lng: coordinates[0]})
-			.query({formatted: 0})
-			.end(function(err, res){
-				if(err) { console.log(err); callback(true); return; }
-				obj = res.body.results;
-				callback(false, obj);				
-			})
-		},		
-		function(callback){
-			request
-			.get('http://api.openweathermap.org/data/2.5/weather')
-			.query({lat:coordinates[1]})
-			.query({lon: coordinates[0]})
-			.query({APPID: appId})
-			.end(function(err, res){
-				if(err) { console.log(err); callback(true); return; }
-				obj = res.body;
-				callback(false, obj);				
-			})
-		}		
-		],
+ function fetch (place, coordinates, i, aggregate) {	
+ 	async.parallel([
+ 		function(callback) {
+ 			request
+ 			.get('https://maps.googleapis.com/maps/api/timezone/json')
+ 			.query('location='+coordinates[1]+','+coordinates[0])
+ 			.query({timestamp: Math.floor(new Date().getTime()/1000)})
+ 			.query({key: gApiToken})
+ 			.end(function(err, res){			
+ 				if(err) { console.log(err); callback(true); return; }
+ 				obj = res.body;
+ 				callback(false, obj);				
+ 			});
+ 		},
+ 		function(callback){
+ 			request
+ 			.get('http://api.sunrise-sunset.org/json')
+ 			.query({lat:coordinates[1]})
+ 			.query({lng: coordinates[0]})
+ 			.query({formatted: 0})
+ 			.end(function(err, res){
+ 				if(err) { console.log(err); callback(true); return; }
+ 				obj = res.body.results;
+ 				callback(false, obj);				
+ 			})
+ 		},		
+ 		function(callback){
+ 			request
+ 			.get('http://api.openweathermap.org/data/2.5/weather')
+ 			.query({lat:coordinates[1]})
+ 			.query({lon: coordinates[0]})
+ 			.query({APPID: appId})
+ 			.end(function(err, res){
+ 				if(err) { console.log(err); callback(true); return; }
+ 				obj = res.body;
+ 				callback(false, obj);				
+ 			})
+ 		}		
+ 		],
   /*
    * Collate results
    */
    function(err, results) {
-   	console.log("Error OR Results", JSON.stringify(err || results));
-   	var data = {   		
-   		'place': place,
-   		'coordinates': [coordinates[1], coordinates[0]],
-   		'timeZoneId': results[0].timeZoneId,
-   		'sunrise': results[1].sunrise,
-   		'sunset': results[1].sunset,
-   		'temperature': results[2].main.temp,
-   		'wind': results[2].wind.speed,
-   		'humidity': results[2].main.humidity,
-   		'icon': results[2].weather[0].icon,
-   		'description': results[2].weather[0].description
-   	};
-   	
-   	if (i == cities.length -1) {
-   		aggregate[place] = data;   		
-   		currentConditions = aggregate   		
-   		pub(currentConditions)
-   	}
-   	else {
-   		++i
-   		console.log('fetching city...', i)
-   		aggregate[place] = data;	
-   		fetch(cities[i], cityCoordinates[cities[i]], i, aggregate)
+   	if (err) {
+   		console.log('Error in fetching from API')
+   		return
+   	} else {   	
+   		try {
+   			console.log("Results", JSON.stringify(results));
+   		var data = {   		
+   			'place': place,
+   			'coordinates': [coordinates[1], coordinates[0]],
+   			'timeZoneId': results[0].timeZoneId,
+   			'sunrise': results[1].sunrise,
+   			'sunset': results[1].sunset,
+   			'temperature': results[2].main.temp,
+   			'wind': results[2].wind.speed,
+   			'humidity': results[2].main.humidity,
+   			'icon': results[2].weather[0].icon,
+   			'description': results[2].weather[0].description
+   		};
+   		
+   		if (i == cities.length -1) {
+   			aggregate[place] = data;   		
+   			currentConditions = aggregate   		
+   			pub(currentConditions)
+   		}
+   		else {
+   			++i
+   			console.log('fetching city...', i)
+   			aggregate[place] = data;	
+   			fetch(cities[i], cityCoordinates[cities[i]], i, aggregate)
+   		}		
+   		} catch (e) {
+   			console.log(e)
+   		}	
+   		
    	}
    }
    );
@@ -132,12 +142,12 @@ setInterval(refreshData, 900000)
  *
  * Fetch happens only at the predefined regular interval
  */
-pubnub.subscribe({
-	channel: "wnPutTime",
-	callback: function(message) {
-		pub(currentConditions)
-	}
-});
+ pubnub.subscribe({
+ 	channel: "wnPutTime",
+ 	callback: function(message) {
+ 		pub(currentConditions)
+ 	}
+ });
 
 /*
  *
@@ -145,18 +155,18 @@ pubnub.subscribe({
  * @param data - current conditions as collated by the fetch method. 
  */
 
-function pub(data) {	
-	pubnub.publish({
-		channel: 'wnGet',
-		message: data,
-		callback: function(m) {
-			console.log(m)
-		}, 
-		error: function(err) {
-			console.log(err)
-		}
-	})
-};
+ function pub(data) {	
+ 	pubnub.publish({
+ 		channel: 'wnGet',
+ 		message: data,
+ 		callback: function(m) {
+ 			console.log(m)
+ 		}, 
+ 		error: function(err) {
+ 			console.log(err)
+ 		}
+ 	})
+ };
 
 //Server begins to execute here
 app.use(express.static('public'))
